35b73e838ef9917739c23f2f1823d759c816d41d
Sanjay <sanjayraoce@gmail.com>
2022-03-15 22:18:18 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,6 @@
+const data =[
+    {name:'sanajay',email:'sanjayraoce@gmail.com'},
+    {name:'sanju',email:'sanjayraoce@gmail.com'},
+    {name:'deepak',email:'deepakce@gmail.com'},
+]
+module.exports =data;
